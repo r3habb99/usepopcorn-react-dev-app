abfdb36bc8d9e5ffd950dedc7a7fb264b7e18097
Rishabh Prajapati <rishabh.prajapati@brainvire.com>
2024-04-15 12:43:06 +0530

    Added new App-v1 for older version , added fetching data with query with search and dynamic data fetching

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-// import './index.css';
-// import App from "./App";
-import StarRating from "./StarRating";
+import "./index.css";
+import App from "./App";
+// import StarRating from "./StarRating";
 
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
+// function Test() {
+//   const [movieRating, setMovieRating] = useState(0);
 
-  return (
-    <div>
-      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
-      <p>This movie was rated {movieRating} stars.</p>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+//       <p>This movie was rated {movieRating} stars.</p>
+//     </div>
+//   );
+// }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
+    <App />
+    {/* <StarRating
       maxRating={5}
       messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
     />
@@ -30,7 +30,7 @@ root.render(
       className="test"
       defaultRating={3}
     />
-    <Test />
+    <Test /> */}
     {/* <StarRating /> */}
   </React.StrictMode>
 );
